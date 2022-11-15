ea4b9ee928379eaeecaa636d3491aebc015870a3
Gyusik Jeong <jgs0663@cheilelec.com>
2022-11-15 13:21:52 +0900

    2강 완료

---
+++ src/main/java/me/sinoroo/pracjwt/entity/Authority.java
@@ -0,0 +1,19 @@
+package me.sinoroo.pracjwt.entity;
+
+import javax.persistence.*;
+
+import lombok.*;
+
+@Entity
+@Table(name = "authority")
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Authority {
+    
+    @Id
+    @Column(name = "authority_name", length = 50)
+    private String authorityName;
+}
